1365c52f41582be4f7b71ff05e0fe411bae1f8ef
stud5nt <serdaniil88@gmail.com>
2025-09-19 11:23:40 +0100

    Update eigth.py

--- eigth/eigth.py
+++ eigth/eigth.py
@@ -8,14 +8,16 @@ class ConfigManager:
     """Context manager for reading and writing JSON configuration"""
 
     def __init__(self, filename):
-        #Initialize
+        """Initialize"""
         self.filename = filename
         self.config = {}
 
     def __enter__(self):
+        """Enter to file"""
         try:
             with open(self.filename, 'r', encoding='utf-8') as file:
                 self.config = json.load(file)
+        """Exception"""
         except FileNotFoundError:
             print(f"Файл з іменем '{self.filename}' не знайдено")
         return self.config
@@ -26,14 +28,15 @@ class ConfigManager:
 
 
 if __name__ == "__main__":
-    #Create config file
+    """Create config file"""
     with open("config.json", "w", encoding="utf-8") as file:
         json.dump({"theme": "dark", "font_size": 12}, file)
         print(json.dumps({"theme": "dark", "font_size": 12}, indent=4))
 
-    #Modify config file
+    """Modify config file"""
     with ConfigManager("config.json") as config:
         config["theme"] = "light"
         config["font_size"] = 10
 
         print(json.dumps(config, indent=4))
+
