f2e6f854bd35eb96319d94744cb11d10873e804a
stud5nt <serdaniil88@gmail.com>
2025-09-19 11:22:26 +0100

    Update fourth.py

--- fourth/fourth.py
+++ fourth/fourth.py
@@ -14,13 +14,14 @@ def filter_file(filename, keyword):
 
 
 if __name__ == "__main__":
-    #Create log file
+    """Create log file"""
     with open("test.log", "w", encoding="utf-8") as file:
         file.write("INFO: TEST\nERROR: ERROR404\nINFO: TEST\nERROR: ERROR500\nLINE: TEST\nERROR: ERROR100")
 
-    #Filter and save lines with keyword
+    """Filter and save lines with keyword"""
     with open("errors.log", "w", encoding="utf-8") as output_file:
         for line in filter_file("test.log", "ERROR"):
             print(line)
             output_file.write(line + "\n")
+
 
